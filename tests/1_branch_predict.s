f2a3e6469aa34e23958f67dc279e9e46e1865c64
grvzard <grvzard@outlook.com>
2024-01-11 14:44:45 +0800

    test: using gcc-mips-linux-gnu tool-chain, add makefile

--- tests/1_branch_predict.s
+++ tests/1_branch_predict.s
@@ -1,13 +1,18 @@
-nop
-addiu $16, $16, 0x5  ; 0x26100005
-nop
-nop
-nop
-nop
-j 0x12  ; 0x0800000c
-addiu $17, $17, 0x1  ; 0x26310001
-nop
-nop
-nop
-nop
-bne $16, $17, -6  ; 0x1611FFFA
+    .set noreorder
+
+_start:
+    nop     
+    addiu   $16,        $16,    0x5         # 0x26100005
+    nop     
+    nop     
+    nop     
+    nop     
+    j       _br_cond                        # 0x0800000C
+_br_body:
+    addiu   $17,        $17,    0x1         # 0x26310001
+    nop     
+    nop     
+    nop     
+    nop     
+_br_cond:
+    bne     $16,        $17,    _br_body    # 0x1611FFFA
--- tests/3_predict_with_forwarding.s
+++ tests/3_predict_with_forwarding.s
@@ -1,5 +1,10 @@
-nop
-addiu $16, $16, 0x5  ; 0x26100005
-j 0x4  ; 0x08000004
-addiu $17, $17, 0x1  ; 0x26310001
-bne $16, $17, -2  ; 0x1611FFFE
+    .set noreorder
+
+_start:
+    nop     
+    addiu   $16,        $16,    0x5         # 0x26100005
+    j       _br_cond                        # 0x08000004
+_br_body:
+    addiu   $17,        $17,    0x1         # 0x26310001
+_br_cond:
+    bne     $16,        $17,    _br_body    # 0x1611FFFE
